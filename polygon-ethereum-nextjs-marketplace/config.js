d5ca67604f41e6b65b9bdc5a44432300c80d5645
chaojin752 <chaojin752@gmail.com>
2021-11-01 09:03:54 +0700

    add cofig

---
+++ polygon-ethereum-nextjs-marketplace/config.js
@@ -0,0 +1,6 @@
+
+// export const nftmarketaddress = "0x0e635a53bb196593A310a7DeFaF6E2a6afdc6D0B"
+// export const nftaddress = "0xbFDC533cB96FbfCA728983dBeE9Df0a6697c4560"
+
+export const nftmarketaddress = "0x1aB03b45F411161764Eb32e861c052cF2918C90C"
+export const nftaddress = "0xD70c756f6c88a099C2cD5b53C4e6fBA1F246C9E6"
